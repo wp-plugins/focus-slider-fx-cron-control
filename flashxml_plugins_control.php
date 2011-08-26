a5a5f83f3dc7720b6dc419eab385b82a4ff7578f
tarnazar <tarnazar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-26 02:35:04 +0000

    tagging version 0.2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/focus-slider-fx-cron-control/trunk@428847 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flashxml_plugins_control.php
+++ flashxml_plugins_control.php
@@ -3,7 +3,7 @@
 Plugin Name: FlashXML plugins CONTROL
 Plugin URI: http://tarnazar.pp.ua/wp_plugins
 Description: Control panel for FlashXML plugins.
-Version: 0.2.0.1
+Version: 0.1.1
 Author: Samael
 Author URI: http://www.tarnazar.pp.ua/wp_plugins
 License: GPL2
@@ -13,21 +13,26 @@ $global_vars = array(
 'plugin_dir_path'=>realpath(dirname(__FILE__)),
 );
 
-function manual_update () {
+function manual_update ($use_comments) {
 global $wpdb;
 global $global_vars;
 $xml = simplexml_load_file($global_vars['plugin_dir_path']."/settings.xml");
     $photos_count = $xml->children()->photos_count;
     $path = $xml->children()->path;
 
-$get_list_sql = $wpdb->get_results("select post_date,guid, post_name,post_title from $wpdb->posts where post_mime_type='image/jpeg' ORDER BY  post_date DESC limit $photos_count;",'ARRAY_A');
+$get_list_sql = $wpdb->get_results("select post_date,guid,post_name,post_title,post_content from $wpdb->posts where post_mime_type='image/jpeg' ORDER BY  post_date DESC limit $photos_count;",'ARRAY_A');
 
 $xml=new DomDocument('1.0','utf-8');
 $images = $xml->appendChild($xml->createElement('images'));
 
 foreach ($get_list_sql as $key => $val) {
    $photo = $images->appendChild($xml->createElement('photo'));
-    $photo->setAttribute('image',$val['guid']);
+   $photo->setAttribute('image',$val['guid']);
+   if ($use_comments == 'on') {
+   $photo->appendChild($xml->createTextNode("<![CDATA[<body>".$val['post_content']."</body>]]>"));
+   }
+   
+    
 }
 
 $xml->formatOutput = true;
@@ -55,12 +60,13 @@ $xml =  simplexml_load_file($global_vars['plugin_dir_path']."/settings.xml");
     }
 }
 
-function save_settings_file ($photos_count,$fp) {
+function save_settings_file ($photos_count,$fp,$use_comments) {
 global $global_vars;
 $xml =  simplexml_load_file($global_vars['plugin_dir_path']."/settings.xml");
 foreach ($xml->xpath('//settings') as $item) {
     $item->photos_count = $photos_count;
     $item->path = $fp;
+    $item->use_comments = $use_comments;
     if ($xml->asXML($global_vars['plugin_dir_path']."/settings.xml")) {
     return true;
     } else 
@@ -80,7 +86,8 @@ $db_user = $xml->children()->db_user;
 $db_pass = $xml->children()->db_pass;
 $db_name = $xml->children()->db_name;
 $db_location = $xml->children()->db_location;
-return array("photos_count"=>$photos_count,"fp"=>$fp,"db_user"=>$db_user,"db_pass"=>$db_pass,"db_name"=>$db_name,"db_location"=>$db_location);
+$use_comments = $xml->children()->use_comments;
+return array("photos_count"=>$photos_count,"fp"=>$fp,"db_user"=>$db_user,"db_pass"=>$db_pass,"db_name"=>$db_name,"db_location"=>$db_location,"use_comments"=>$use_comments);
 }
 
 function admin_control() {
@@ -89,9 +96,10 @@ function admin_control() {
 
 function fsfx_cc_options () {
 global $wpdb;
+global $global_vars;
 $lsf = load_settings_file();
 if ($_GET['settings-updated']) {
-save_settings_file (get_option('photos_count'),get_option('path'));
+save_settings_file (get_option('photos_count'),get_option('path'),get_option('use_comments'));
 }
 if (($lsf['db_user']==$wpdb->dbuser) and ($lsf['db_pass']==$wpdb->dbpassword) and ($lsf['db_name']==$wpdb->dbname) and ($lsf['db_location']==$wpdb->dbhost)) {
 echo "Database Settings is Ok!";
@@ -101,7 +109,8 @@ echo "Database configuration was wrong. But now its Ok";
 }
 }
 if (isset($_GET['do_manual'])) {
-manual_update();
+manual_update($lsf['use_comments']);
+
 echo "<br>Updated!";
 }
 ?>
@@ -116,14 +125,18 @@ echo "<br>Updated!";
         You might change the path to the file from which Focus Slider FX takes necessary adjustments.
         The number of pictures displaying on the page is also variable. 
         Focus Slider FX plug-in doesn't came with this package. It should be installed separately.
-        The Cron Job is writing manually as it is * * * * * cd /path/to/wp-plugin-dir/t-slider-fx_cron_control; /path/to//php gen_flashxml.php 1>/dev/null 2/dev/null.
-        
+        The Cron Job is writing manually as it is <b>* * * * * cd <?php echo $global_vars['plugin_dir_path'];?>; <?php echo exec('which php');?> gen_flashxml.php 1>/dev/null 2/dev/null </b>.
+
+	- Now you can use comments for your pictures. You can input your comments in the medial library.
+	
+	        
         </pre> 
         Last photos count: <input type="text" name="photos_count" value="<?php echo $lsf["photos_count"];?>" size=2><p>
-        DEFAULT PATH: <b> <?php echo WP_CONTENT_DIR?>/flashxml/focus-slider-fx/images.xml</b><br>
+        DEFAULT PATH: <b> <?php echo WP_CONTENT_DIR?>/flashxml/images.xml</b><br>
         Path To images.xml: <input type="text" name="path" value="<?php echo $lsf["fp"]; ?>" size=100><br>
+	<input type="checkbox" name="use_comments" <?php if ($lsf['use_comments']=='on') { echo "checked"; } ?>> Use comments from media library<br>
         <input type="hidden" name="action" value="update" />
-        <input type="hidden" name="page_options" value="photos_count,path" />
+        <input type="hidden" name="page_options" value="photos_count,path,use_comments" />
 
         <p class="submit">
           <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
